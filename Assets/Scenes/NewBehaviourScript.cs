340f9db78994a3fe0a7ddaf9f3504bf980bd1c38
linkailin <1287375661@qq.com>
2019-12-23 16:43:33 +0800

    one
    one

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(GetHttpInfo());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public Text text;
+
+    int Zhangjie_num = 0;//实际跟Index相加的章节数
+    int Index_num=12533506;//第一章的index
+    int ShowZhangjie_num = 0;//用来查询和命名txt的章节数
+    IEnumerator GetHttpInfo () {
+
+        //www获取资源，要求网页的index必须是按整数+1，大多数网站都是这样的，别担心
+        WWW url = new WWW ("http://www.siluke.com/0/11/11477/"+(Index_num+Zhangjie_num)+".html");
+
+        yield return url;
+
+        byte[] byt = url.bytes;
+        //过小的为空章节（思路客大章节之间用空章节隔开，此处用两个变量自加区分开，）
+        if (byt.Length < 200)
+        {
+            Zhangjie_num++;
+ 
+            if (Zhangjie_num < 582)
+            {
+                StartCoroutine(GetHttpInfo());
+            }
+        }
+        else
+        {
+            //将byte转为utf8格式
+            byt = Encoding.Convert(Encoding.GetEncoding("GBK"), Encoding.UTF8, byt);
+            string str = Encoding.UTF8.GetString(byt);
+            Debug.Log(str);
+
+            //下面的操作是针对实际文本中筛选获取需要的文本
+            //仔细阅读上面debug出来的string，找到想要的规则，尽量找网页之间共性的，单个网页内重复性低的关键字符串
+            //思路客的规则大概是正文"&nbsp;&nbsp;&nbsp;&nbsp;"开头，"<script>read_link_up()"结尾
+            string str_start = "&nbsp;&nbsp;&nbsp;&nbsp;";
+            string str_end = "<script>read_link_up()";
+            int IndexofA = str.IndexOf(str_start);
+            int IndexofB = str.LastIndexOf(str_end);
+            //从str_start开始取，要把自己的字符长度算进去，到str_end的头减去前面的部分，再往前推，所以每本书这里的数值要自己改，如果此处有更好更通用的算法欢迎评论
+            string finalstr = str.Substring(IndexofA + str_start.Length, IndexofB - IndexofA - 32);
+            //下面就是把正文中一些垃圾字符拿移除，设置好文本间的换行
+            finalstr = finalstr.Replace("&nbsp;&nbsp;&nbsp;&nbsp;", "\n");
+            finalstr = finalstr.Replace("&nbsp;", "");
+            finalstr = finalstr.Replace("<br /><br />", "");
+
+            //Debug.Log((str.Substring(400)));
+            //Debug.Log(finalstr);
+
+
+            //这里作者获取了一下文章的标题，放入整个string的最前面，原理参照如上
+            string str_start_title = "title";
+            string str_end_title = "/title";
+            int IndexofA_title = str.IndexOf(str_start_title);
+            int IndexofB_title = str.IndexOf(str_end_title);
+            string finalstr_title = str.Substring(IndexofA_title + str_start_title.Length + 1, IndexofB_title - IndexofA_title - 14);
+            //Debug.Log(finalstr_title);
+            finalstr = finalstr_title + "\n" + finalstr;
+
+            //此处用来显示string拿来debug看看
+            text.text = finalstr;
+
+            //接下来就跟之前一样了，写入文件，新建个文件夹分章存，参考斗破，此处没写文件夹新建逻辑，自己去弄文件夹吧，或者你自己写判断创建文件夹
+            string BookName = "sw";
+            string BookUrl = Application.streamingAssetsPath + "/" + BookName; ;//弄一个文件夹，用来存放一千多个章节的txt
+            string ZhangJieUrl = BookUrl + "/第" + (ShowZhangjie_num + 1) + "章.txt";//给每一章一个名字吧，方便加载
+            if (!File.Exists(ZhangJieUrl))
+            {//把分别把每一章写进去创建成txt
+                FileStream fs1 = new FileStream(ZhangJieUrl, FileMode.Create, FileAccess.Write);//创建写入文件 
+                StreamWriter sw = new StreamWriter(fs1);
+                sw.WriteLine(finalstr);//开始写入值
+                sw.Close();
+                sw.Dispose();
+                fs1.Close();
+                fs1.Dispose();
+                //注意close和dispose，不释放unity会卡死
+            }
+
+            Zhangjie_num++;
+            ShowZhangjie_num++;
+            yield return 1;
+            //写完一章debug一下，让你不至于那么焦躁，最终写完了告诉你allend，此处的582，是拿最后一章的index减去第一章的index算出来的，应该还要+1，不然会缺失最后一章
+            Debug.Log("Write_end" + Zhangjie_num);
+            if (Zhangjie_num < 582)
+            {
+                StartCoroutine(GetHttpInfo());
+            }
+            else
+            {
+                Debug.Log("Allend!!!!!!!!!");
+            }
+        }
+    }
+
+
+
+}
---
+++ Assets/Scenes/readtext.cs
@@ -0,0 +1,239 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using System;
+using UnityEngine.EventSystems;
+
+  
+public class readtext : MonoBehaviour
+{
+	
+	int HangZiShu = 0;//每行多少字
+	int YeHangShu = 0;//每页多少行
+
+	int CurrentZhangJie = 0;//当前章节数
+	int CurrentYeShu = 0;//当前章节数的页数
+
+	public Text text;//UI text,可以将读取的txt在UI中显示出现
+	string book = "";//用来存从txt中获取的string
+
+	void Start ()
+	{  
+		Init ();
+		//PlayerPrefs.DeleteAll ();
+		CurrentZhangJie = PlayerPrefs.GetInt ("CurrentZhangJie");//获取存储的章节数
+		CurrentYeShu = PlayerPrefs.GetInt ("CurrentYeShu");//获取页数
+//		CreatTxt ("dpcq");
+		FanZhang ();//翻到该章
+		FanYe ();//翻到该页
+	}
+
+	void Init ()
+	{
+		int screenW = 1080;
+		int screenH = 1920;
+	
+		//获取每一行能显示多少字
+		HangZiShu = (int)(screenW - (Math.Abs (text.rectTransform.offsetMax.x) + Math.Abs (text.rectTransform.offsetMin.x))) / text.fontSize;
+		//获取每一页能显示多少行
+		YeHangShu = (int)((screenH - (Math.Abs (text.rectTransform.offsetMax.y) + Math.Abs (text.rectTransform.offsetMin.y))) / text.fontSize / 1.1f);
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonUp (0)) {
+			if (Input.mousePosition.y / Screen.height < 0.8f) {
+				if (Input.mousePosition.x / Screen.width < 0.6f && Input.mousePosition.x / Screen.width > 0.4f) {
+					//这个是给点击中间预留的菜单栏呼出
+					OpenCloseOtherObj (!IsOpen);
+				} else {
+					if (Input.mousePosition.x / Screen.width > 0.666f) {
+						CurrentYeShu++;
+					} else {
+						CurrentYeShu--;
+					}
+					if (CurrentYeShu < 0) {
+						//上一章最后一页
+						if (CurrentZhangJie == 0) {
+							CurrentYeShu = 0;
+							return;
+						} else {
+							CurrentZhangJie--;
+							FanZhang ();
+							CurrentYeShu = TempYeNum;
+							FanYe ();
+						}
+					} else if (TempYe [CurrentYeShu] == "") {
+						//下一章第一页
+						CurrentZhangJie++;
+						FanZhang ();
+						CurrentYeShu = 0;
+						FanYe ();
+					} else {
+						//本章翻页
+						FanYe ();
+					}
+				}
+			}
+		}
+	}
+
+    public void ReadNext(){
+        CurrentYeShu++;
+        if (CurrentYeShu < 0)
+        {
+            //上一章最后一页
+            if (CurrentZhangJie == 0)
+            {
+                CurrentYeShu = 0;
+                return;
+            }
+            else
+            {
+                CurrentZhangJie--;
+                FanZhang();
+                CurrentYeShu = TempYeNum;
+                FanYe();
+            }
+        }
+        else if (TempYe[CurrentYeShu] == "")
+        {
+            //下一章第一页
+            CurrentZhangJie++;
+            FanZhang();
+            CurrentYeShu = 0;
+            FanYe();
+        }
+        else
+        {
+            //本章翻页
+            FanYe();
+        }
+    }
+
+	void FanYe ()
+	{
+		//翻页对text进行赋值，然后保存一下
+		text.text = TempYe [CurrentYeShu];
+		PlayerPrefs.SetInt ("CurrentYeShu", CurrentYeShu);
+	}
+
+	//使用20个页来显示一章，每一个TempYe就是用来储存一页的内容
+	string[] TempYe = new string[40];
+	int TempYeNum = 0;
+	int TempYeLine = 0;
+	void FanZhang ()
+	{
+		ClearTempYe ();//清空TempYe数组
+		var fileAddress = System.IO.Path.Combine (Application.streamingAssetsPath, "sw" + "/第" + (CurrentZhangJie + 1) + "章.txt");  
+		FileInfo fInfo0 = new FileInfo (fileAddress);  
+		string s = "";  
+		if (fInfo0.Exists) {  
+			StreamReader r = new StreamReader (fileAddress);  
+			book = r.ReadToEnd (); 
+		}
+		string[] SplitStr = { "\n" };
+
+		//以行分割一章内容
+		string[] Line = book.Split (SplitStr, System.StringSplitOptions.None);
+		TempYeNum = 0;
+		TempYeLine = 0;
+		//遍历每段
+		for (int i = 0; i < Line.Length; i++) {
+			//获取这一段可以在显示端分为多少行
+			int lineih = (int)Math.Ceiling ((double)Line [i].Length / HangZiShu);
+
+			for (int j = 0; j < lineih; j++) {
+				//逐行赋值，该页一共能赋值HangZiShu行，HangZiShu满后赋值下一个TempYe
+				if (j < lineih - 1)
+					TempYe [TempYeNum] += Line [i].Substring (j * HangZiShu, HangZiShu);
+				else
+					TempYe [TempYeNum] += Line [i].Substring (j * HangZiShu);
+				TempYeLine++;
+				if (TempYeLine >= YeHangShu-1) {
+					TempYeNum++;
+					TempYeLine = 0;
+				}
+			}
+			//当不为该页首行的时候每段要分段
+			if (TempYeLine != 0)
+				TempYe [TempYeNum] += "\r\n";
+		}
+
+		PlayerPrefs.SetInt ("CurrentZhangJie", CurrentZhangJie);
+
+	}
+
+	//清理页资源
+	void ClearTempYe ()
+	{
+		for (int i = 0; i < TempYe.Length; i++) {
+			TempYe [i] = "";
+		}
+	}
+
+
+		
+
+
+
+	public InputField InputField;
+	public GameObject[] OtherObj;
+	bool IsOpen = false;
+
+	public void GoToZhangJie ()
+	{
+		Debug.Log (InputField.text);
+		CurrentZhangJie = Convert.ToInt32 (InputField.text) - 1;
+		FanZhang ();
+		CurrentYeShu = 0;
+		FanYe ();
+		OpenCloseOtherObj (false);
+	}
+
+	void OpenCloseOtherObj (bool isopen)
+	{
+		IsOpen = isopen;
+		foreach (GameObject gm in OtherObj) {
+			gm.SetActive (IsOpen);
+		}
+	}
+
+	void CreatTxt (string BookName)
+	{
+		string BookUrl = Application.streamingAssetsPath + "/" + BookName;//弄一个文件夹，用来存放一千多个章节的txt
+		if (!Directory.Exists (BookUrl)) {//判断该路径是否存在，如果没有就创建路径，如果有就说明已经创建过啦
+			Directory.CreateDirectory (BookUrl);//创建路径
+			//判读需要拆解的txt是否存在，如果有就ReadToEnd，获取整本书txt全部的string
+			var fileAddress = System.IO.Path.Combine (Application.streamingAssetsPath, BookName + ".txt");  
+			FileInfo fInfo0 = new FileInfo (fileAddress);  
+			string s = "";  
+			if (fInfo0.Exists) {  
+				StreamReader r = new StreamReader (fileAddress);  
+				book = r.ReadToEnd (); 
+			}  
+
+			//这里是文本txt的分隔符，就是两个回车键，这是我下载的斗破苍穹的分隔符，至于其他书籍是否是用这个，还需要你们自己调研啦
+			string[] SplitStr = { "\r\n\r\n" };
+			string[] ZhangJie = book.Split (SplitStr, System.StringSplitOptions.None);//把整个string拆解成一个个string最终变成数组
+			int num = 0;//用于记录章节数
+			foreach (string ZhangJieNeiRong in ZhangJie) {
+				if (ZhangJieNeiRong.Length > 120) {//实际过程中会出现空章节，用一个长度来过滤掉太短的章节，当然也不一定要是120，可以根据需要设置
+					num++;
+					string ZhangJieUrl = BookUrl + "/第" + num + "章.txt";//给每一章一个名字吧，方便加载
+					if (!File.Exists (ZhangJieUrl)) {//把分别把每一章写进去创建成txt
+						FileStream fs1 = new FileStream (ZhangJieUrl, FileMode.Create, FileAccess.Write);//创建写入文件 
+						StreamWriter sw = new StreamWriter (fs1);
+						sw.WriteLine (ZhangJieNeiRong);//开始写入值
+						sw.Close ();
+						fs1.Close ();
+					}
+				}
+			}
+		}
+	}
+
+} 
